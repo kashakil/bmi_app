92246d06f752ee33bdfe4002da7ad63581b7640d
Kawsar Ahmed Shakil <kashakil75@gmail.com>
2023-01-17 21:44:59 +0600

    ui completed

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -26,6 +26,86 @@ class _HomeScreenState extends State<HomeScreen> {
               height: 85,
               width: 75,
             ),
+            Container(
+              margin: const EdgeInsets.all(3),
+              height: 245,
+              color: Colors.grey.shade300,
+              child: Column(
+                children: [
+                  const TextField(
+                    cursorColor: null,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Age',
+                      hintText: 'e.g: 34',
+                      icon: Icon(Icons.person_outline),
+                    ),
+                  ),
+                  const TextField(
+                    cursorColor: null,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Height in Feet',
+                      hintText: 'e.g: 5.6',
+                      icon: Icon(Icons.insert_chart),
+                    ),
+                  ),
+                  const TextField(
+                    cursorColor: null,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Weight in lbs',
+                      hintText: 'e.g: 180 ',
+                      icon: Icon(Icons.line_weight),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(20),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.pinkAccent,
+                        elevation: 0,
+                      ),
+                      child: const Text(
+                        'Calculate',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              children: const [
+                Text(
+                  'BMI: ',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.blueAccent,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(5),
+                ),
+                Text(
+                  'Overweight: ',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.pinkAccent,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
